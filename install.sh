287b19ee089e4c8aa1e3794e4e6ebf83da0084d1
Brad <bradtaniguchi@gmail.com>
2021-06-30 16:14:42 -0700

    Add git setup to install.sh file

--- install.sh
+++ install.sh
@@ -14,3 +14,11 @@ echo "alias vim='nvim'" >> ~/.zshrc
 # set editors to nvim
 echo "export VISUAL=nvim" >> ~/.bashrc
 echo "export EDITOR=\"$VISUAL\"" >> ~/.bashrc
+
+
+# git setup
+git config --global core.editor "nim"
+
+# setup git completion
+echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
+echo "source /usr/share/bash-completion/completions/git" >> ~/.zshrc
