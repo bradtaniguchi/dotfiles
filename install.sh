81e8acff020ac032ce384dd7a408848d399e0ffa
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-02-21 21:51:28 -0800

    chore: fix install trying to install vimrc

--- install.sh
+++ install.sh
@@ -23,7 +23,6 @@ fi
 # Copy run-com files if they don't already exist
 echo ">> Copying run-com files"
 cp -n ./runcom/.tmux.conf ~/.tmux.conf
-cp -n ./runcom/.vimrc ~/.vimrc
 cp -n ./runcom/init.vim ~/.config/nvim/init.vim
 
 # Copy aliases
