2b291235d12ec445934a36bc934f849c7a01ad6e
Brad <bradtaniguchi@gmail.com>
2024-03-23 17:05:49 +0000

    chore: remove nvm, move back to vim

--- install.sh
+++ install.sh
@@ -12,13 +12,17 @@ echo ">> Copying system aliases"
 cat ./system/.rc >> ~/.zshrc
 cat ./system/.rc >> ~/.bashrc
 
-# git setup
-echo ">> Setting global git editor to nvim, if exists"
-if command -v nvim &> /dev/null
-then
-    echo "nvm found, setting global git editor"
-    git config --global core.editor "nvim"
-fi
+
+# comment out, to move away from nvm for stability sake
+# echo ">> Setting global git editor to nvim, if exists"
+# if command -v nvim &> /dev/null
+# then
+#     echo "nvm found, setting global git editor"
+#     git config --global core.editor "vim"
+# fi
+
+echo ">> Setting global git editor to vim"
+git config --global core.editor "vim"
 
 # Copy run-com files if they don't already exist
 echo ">> Copying run-com files"
@@ -30,18 +34,15 @@ cat ./system/.aliases >> ~/.zshrc
 cat ./system/.aliases >> ~/.bashrc
 
 # Setup nvim and helpers
-apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install --no-install-recommends neovim wget gnupg curl \
-    && apt-get clean
 
 
-echo ">> installing nvchad"
+# echo ">> installing nvchad"
 # if this has issues check here:
 # https://nvchad.com/quickstart/install
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+# git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 
 # setup vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo ">> Done!"
