b4f51745b6a9702e6d277651019acf45f0f94a75
Brad Taniguchi <bradtaniguchi@gmail.com>
2025-08-27 20:19:29 -0700

    feat: add separate install

--- install.js
+++ install.js
@@ -13,56 +13,106 @@ import { verifyManualInstallation } from './src/verify/verify-manual-installatio
   try {
     const program = new Command();
 
-    program
-      .name('install')
-      .description('Install dotfiles')
-      .option('-d, --dry-run', 'Run the installation in dry-run mode', false)
-      .option('-p, --parallel', 'Run installations in parallel', false)
-      .option('--plugins', 'Install plugins for neo-vim', false)
-
-      .option('-f, --force', 'Force overwrite of existing files', false)
-      .helpOption('-h, --help', 'Show this help message')
-      .parse();
-
-    const options = program.opts();
-
-    const dryRun = options.dryRun;
-    const parallel = options.parallel;
-    const installPlugins = options.plugins;
-    const force = options.force;
-
-    log('starting install, with args', { dryRun, force, HOME_DIR: ENV.HOME });
-
-    const manualInstallation$ = verifyManualInstallation(dryRun);
-
-    const installRuncom$ = installRuncom(dryRun, { force });
-
-    const installGitConfigs$ = installGitConfigs(dryRun, { force });
-
-    const installTmuxConfig$ = installTmuxConfig(dryRun, { force });
-
-    const installNvimConfig$ = installNvimConfig(dryRun, {
-      installPlugins,
-      force,
-    });
-
-    if (parallel) {
-      await Promise.all([
-        manualInstallation$,
-        installRuncom$,
-        installGitConfigs$,
-        installTmuxConfig$,
-        installNvimConfig$,
-      ]);
-    } else {
-      await manualInstallation$;
-      await installRuncom$;
-      await installGitConfigs$;
-      await installTmuxConfig$;
-      await installNvimConfig$;
+    // Shared options for all commands
+    function addCommonOptions(cmd) {
+      return cmd
+        .option('-d, --dry-run', 'Run the installation in dry-run mode', false)
+        .option('-f, --force', 'Force overwrite of existing files', false);
     }
 
-    log('done installing dotfiles!');
+    // Main install command
+    addCommonOptions(program)
+      .name('install')
+      .description('Install dotfiles')
+      .option('-p, --parallel', 'Run installations in parallel', false)
+      .option('--plugins', 'Install plugins for neo-vim', false)
+      .helpOption('-h, --help', 'Show this help message')
+      .action(
+        async (
+          /** @type {{ dryRun: any; parallel: any; plugins: any; force: any; }} */ options,
+        ) => {
+          const dryRun = options.dryRun;
+          const parallel = options.parallel;
+          const installPlugins = options.plugins;
+          const force = options.force;
+
+          log('starting install, with args', {
+            dryRun,
+            force,
+            HOME_DIR: ENV.HOME,
+          });
+
+          const manualInstallation$ = verifyManualInstallation(dryRun);
+          const installRuncom$ = installRuncom(dryRun, { force });
+          const installGitConfigs$ = installGitConfigs(dryRun, { force });
+          const installTmuxConfig$ = installTmuxConfig(dryRun, { force });
+          const installNvimConfig$ = installNvimConfig(dryRun, {
+            installPlugins,
+            force,
+          });
+
+          if (parallel) {
+            await Promise.all([
+              manualInstallation$,
+              installRuncom$,
+              installGitConfigs$,
+              installTmuxConfig$,
+              installNvimConfig$,
+            ]);
+          } else {
+            await manualInstallation$;
+            await installRuncom$;
+            await installGitConfigs$;
+            await installTmuxConfig$;
+            await installNvimConfig$;
+          }
+
+          log('done installing dotfiles!');
+        },
+      );
+
+    // Subcommand: runcom
+    addCommonOptions(
+      program.command('runcom').description('Install only runcom config'),
+    ).action(async (/** @type {{ dryRun: any; force: any; }} */ options) => {
+      const dryRun = options.dryRun;
+      const force = options.force;
+      log('installing runcom config', { dryRun, force });
+      await installRuncom(dryRun, { force });
+      log('done installing runcom config!');
+    });
+
+    // Subcommand: tmux
+    addCommonOptions(
+      program.command('tmux').description('Install only tmux config'),
+    ).action(async (/** @type {{ dryRun: any; force: any; }} */ options) => {
+      const dryRun = options.dryRun;
+      const force = options.force;
+      log('installing tmux config', { dryRun, force });
+      await installTmuxConfig(dryRun, { force });
+      log('done installing tmux config!');
+    });
+
+    // Subcommand: neovim
+    addCommonOptions(
+      program
+        .command('neovim')
+        .description('Install only neovim config')
+        .option('--plugins', 'Install plugins for neo-vim', false),
+    ).action(
+      async (
+        /** @type {{ dryRun: any; force: any; plugins: any; }} */ options,
+      ) => {
+        const dryRun = options.dryRun;
+        const force = options.force;
+        const installPlugins = options.plugins;
+        log('installing neovim config', { dryRun, force, installPlugins });
+        await installNvimConfig(dryRun, { installPlugins, force });
+        log('done installing neovim config!');
+      },
+    );
+
+    await program.parseAsync();
   } catch (err) {
     log('Error installing dotfiles: ', err);
     process.exit(1);
